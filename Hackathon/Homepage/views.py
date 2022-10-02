4a060fb9120145b186614dfae2ac52678ec6b5ae
LeileiJng <56954752+leileijng@users.noreply.github.com>
2022-10-02 10:42:52 +0800

    jl

--- Hackathon/Homepage/views.py
+++ Hackathon/Homepage/views.py
@@ -16,15 +16,6 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
-
-#Affindi Endpoint
-SIGNUP = "https://cloud-wallet-api.prod.affinity-project.org/api/v1/users/signup"
-SIGNUPCONFIRM = "https://cloud-wallet-api.prod.affinity-project.org/api/v1/users/signup/confirm"
-LOGIN = 'https://cloud-wallet-api.prod.affinity-project.org/api/v1/users/login'
-#API key
-API_KEY = {'Api-Key' :"ede388293715a2b2da022caea7819beb5a61eb72e3b58d92b472112663b86055",
-           "Content-Type": "application/json"}
-
   
 # sending post request and saving response as response object
 #r = requests.post(url = API_ENDPOINT, data = data)
@@ -38,8 +29,6 @@ def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if(form.is_valid()):
-            
-
             #Payload = {
             #    "username": form.cleaned_data['Email'],
             #    "password": form.cleaned_data['Password']
@@ -49,16 +38,12 @@ def login(request):
             print(form.cleaned_data['Choice'])
             if(form.cleaned_data['Choice'] == '1'):
                 for Data in Company.objects.all():
-                    if(Data.Email == form.cleaned_data['Email']):
-                        if(Data.Password == form.cleaned_data['Password']):
-                            return HttpResponseRedirect('company')
+                    return HttpResponseRedirect('home')
                 msg = 'Wrong Email/Password'
                 
             else:
                 for Data in Individual.objects.all():
-                    if(Data.Email == form.cleaned_data['Email']):
-                        if(Data.Password == form.cleaned_data['Password']):
-                            return HttpResponseRedirect('user')
+                    return HttpResponseRedirect('home')
                 msg = 'Wrong Email/Password'
     else:
         form = LoginForm()
@@ -84,21 +69,13 @@ def signupuser(request):
         
     return render(request, 'individual_signup.html', {'form' : form})
 Token = ""
+
 def signupcompany(request):
     if request.method == 'POST':
         form = CompanySignupForm(request.POST)
         if(form.is_valid()):
-            Payload = {
-                "username": form.cleaned_data['Email'],
-                "password": form.cleaned_data['Password']
-            }
-            print(form.cleaned_data['Email'], form.cleaned_data['Password'])
-            APIresponse = requests.post(url = SIGNUP, json = Payload, headers = API_KEY)
-            global Token 
-            Token = APIresponse.text
-            print(Token)
             form.save()
-            return HttpResponseRedirect('verification')
+            return HttpResponseRedirect('login')
         else:
             print(form.errors)
     else:
@@ -204,3 +181,6 @@ def truckingRequest(request):
 
 def forkliftServices(request):
     return render(request, 'forkliftServices.html')
+
+def home(request):
+    return render(request, 'home.html')
